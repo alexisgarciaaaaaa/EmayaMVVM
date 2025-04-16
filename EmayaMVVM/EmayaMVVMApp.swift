44c6e08d7ff6de25ed1ef84bfc763c68b63b4d20
bryangarcia <alexisgarciathb1996@gmail.com>
2025-04-15 18:56:43 -0600

    Initial Commit

---
+++ EmayaMVVM/EmayaMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmayaMVVMApp.swift
+//  EmayaMVVM
+//
+//  Created by bryangarcia on 15/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EmayaMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
